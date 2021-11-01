ff84bd98ac89585d9e0cda981eed487149b4b1b6
Federico <fdmartinez44@gmail.com>
2021-10-31 23:30:47 -0300

    se agrego url de repo de git

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="fixed-bottom">
         <nav class="navbar navbar-light bg-light">
             <div class="container-fluid">
-            <a class="" href="#">Watch this code <i class='bx bxl-github'></i></a>
+            <a class="" href="https://fdmarti.github.io/randomwords/">Watch this code <i class='bx bxl-github'></i></a>
             </div>
         </nav>
     </div>
