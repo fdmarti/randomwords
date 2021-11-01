42d6d75ca8ad2c53da7459a0682914455b2cdd45
Federico <fdmartinez44@gmail.com>
2021-10-31 23:31:41 -0300

    se cambio url de github

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="fixed-bottom">
         <nav class="navbar navbar-light bg-light">
             <div class="container-fluid">
-            <a class="" href="https://fdmarti.github.io/randomwords/">Watch this code <i class='bx bxl-github'></i></a>
+            <a class="" href="https://github.com/fdmarti/randomwords">Watch this code <i class='bx bxl-github'></i></a>
             </div>
         </nav>
     </div>
